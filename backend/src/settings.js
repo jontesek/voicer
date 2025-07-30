b696316c32e34490c2c61d142afa4e574071e6ad
Jonáš Petrovský <jontesek@gmail.com>
2025-07-30 11:52:18 +0200

    backend: add docker for api

--- backend/src/settings.js
+++ backend/src/settings.js
@@ -1,4 +1,4 @@
 export const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
-export const DB_FILE_PATH = './../files/db/voicer.sqlite';
+export const DB_FILE_PATH = process.env.DB_FILE_PATH || './../files/db/voicer.db';
 export const VOICER_BUCKET = 'voicer';
 export const FILES_PATH = 'generated_files';
