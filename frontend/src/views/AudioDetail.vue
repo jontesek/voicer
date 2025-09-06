153a1d979af32bfb4c40cb7dec769110cb5cd934
Jonáš Petrovský <jontesek@gmail.com>
2025-09-06 20:16:18 +0200

    fix(AudioDetail): disable form fields

--- frontend/src/views/AudioDetail.vue
+++ frontend/src/views/AudioDetail.vue
@@ -20,18 +20,18 @@
                     <p class="mb-1 fw-bold">Speech model:</p>
                     <div class="form-check">
                         <input class="form-check-input" type="radio" v-model="audioForm.model" id="speechBasicOption"
-                            value="basic">
+                            value="basic" disabled>
                         <label class="form-check-label" for="aqBasicOption" checked>Basic</label>
                     </div>
                     <div class="form-check">
                         <input class="form-check-input" type="radio" v-model="audioForm.model" id="speechAdvancedOption"
-                            value="advanced">
+                            value="advanced" disabled>
                         <label class="form-check-label" for="aqAdvancedOption">Advanced</label>
                     </div>
                 </div>
                 <div class="mb-3">
                     <label for="voiceSelect" class="me-1 fw-bold">Voice name:</label>
-                    <select id="voiceSelect" class="form-select" v-model="audioForm.voiceName">
+                    <select id="voiceSelect" class="form-select" v-model="audioForm.voiceName" disabled>
                         <option v-for="voice in voices" :key="voice.name" :value="voice.name">
                             {{ voice.name }} - {{ voice.mood }}, {{ voice.pitch }} pitch
                         </option>
@@ -40,46 +40,46 @@
                 <div class="mb-3">
                     <label for="audioTemp" class="form-label"><strong>Temperature</strong> (expresivity):</label>
                     <input type="range" class="form-range w-25 ms-2 me-2 pt-3" id="audioTemp" min="0" max="2"
-                        step="0.05" value="1" v-model="audioForm.temperature">
+                        step="0.05" value="1" v-model="audioForm.temperature" disabled>
                     <span id="audioTempValue">{{ audioForm.temperature }}</span>
                 </div>
                 <div class="mb-3">
                     <label for="audioStyle" class="form-label"><strong>Style instructions</strong> (optional):</label>
-                    <textarea class="form-control" id="audioStyle" v-model="audioForm.style"></textarea>
+                    <textarea class="form-control" id="audioStyle" v-model="audioForm.style" disabled></textarea>
                 </div>
                 <div class="mb-3">
                     <label for="audioText" class="form-label fw-bold">Text:</label>
-                    <textarea class="form-control" id="audioText" v-model="audioForm.text" required></textarea>
+                    <textarea class="form-control" id="audioText" v-model="audioForm.text" required disabled></textarea>
                 </div>
             </form>
-                <div class="mb-3">
-                    <h4>Metadata</h4>
-                    <ul>
-                        <li>Input tokens: {{ audioForm.inputTokenCount }}</li>
-                        <li>Output tokens: {{ audioForm.outputTokenCount }}</li>
-                        <li>Audio duration: {{ formatDuration(audioForm.audioDuration) }}</li>
-                        <li>Generated in: {{ formatDuration(audioForm.generationDuration) }}</li>
+            <div class="mb-3">
+                <h4>Metadata</h4>
+                <ul>
+                    <li>Input tokens: {{ audioForm.inputTokenCount }}</li>
+                    <li>Output tokens: {{ audioForm.outputTokenCount }}</li>
+                    <li>Audio duration: {{ formatDuration(audioForm.audioDuration) }}</li>
+                    <li>Generated in: {{ formatDuration(audioForm.generationDuration) }}</li>
+                </ul>
+            </div>
+            <div v-if="audioFileTmpUrl" class="mb-3">
+                <h4>Audio file</h4>
+                <div class="audio-file">
+                    <audio controls>
+                        <source :src="audioFileTmpUrl" type="audio/ogg">
+                    </audio>
+                    <ul class="download-links">
+                        <li><a :href="audioFileTmpUrl" :download="downloadFileName">Download Ogg</a></li>
+                        <li><a href="#" :download="downloadFileName" @click.prevent="downloadSound('mp3')">Download
+                                MP3</a></li>
+                        <li><a href="#" :download="downloadFileName" @click.prevent="downloadSound('wav')">Download
+                                WAV</a></li>
                     </ul>
                 </div>
-                <div v-if="audioFileTmpUrl" class="mb-3">
-                    <h4>Audio file</h4>
-                    <div class="audio-file">
-                        <audio controls>
-                            <source :src="audioFileTmpUrl" type="audio/ogg">
-                        </audio>
-                        <ul class="download-links">
-                            <li><a :href="audioFileTmpUrl" :download="downloadFileName">Download Ogg</a></li>
-                            <li><a href="#" :download="downloadFileName" @click.prevent="downloadSound('mp3')">Download
-                                    MP3</a></li>
-                            <li><a href="#" :download="downloadFileName" @click.prevent="downloadSound('wav')">Download
-                                    WAV</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="mt-4">
+            </div>
+            <div class="mt-4">
                 <button @click="updateAudio" type="button" class="btn btn-primary me-5">Update title</button>
-                    <button @click="deleteAudio" type="button" class="btn btn-danger">Delete audio</button>
-                </div>
+                <button @click="deleteAudio" type="button" class="btn btn-danger">Delete audio</button>
+            </div>
         </div>
     </div>
 </template>
