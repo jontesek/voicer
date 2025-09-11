49d778ea56d0da54789928e600ef8d2ecf527356
Jonáš Petrovský <jontesek@gmail.com>
2025-09-11 15:00:17 +0200

    refactor(backend): use correct api host name

--- backend/src/settings.js
+++ backend/src/settings.js
@@ -1,6 +1,8 @@
-export const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+export const GEMINI_API_KEY = process.env.GEMINI_API_KEY;   // placed in /.env
 export const DB_FILE_PATH = process.env.DB_FILE_PATH || './../files/db/voicer.db';
 export const VOICER_BUCKET = 'voicer';
 export const FILES_PATH = 'generated_files';
-export const CONVERTER_API_URL = 'http://localhost:3001';
-export const MINIO_HOST = 'localhost';
+// conversion when run via docker, localhost otherwise
+export const CONVERTER_API_URL = `http://${process.env.CONVERSION_HOST}:3001`;
+// minio when run via docker, localhost otherwise
+export const MINIO_HOST = process.env.MINIO_HOST
