0204de135f5e8e61659bb17b4ff82a84c76cddbd
Jonáš Petrovský <jontesek@gmail.com>
2025-09-12 11:03:02 +0200

    refactor(backend): docker-compose.yml: change minio password, run only minio

--- backend/src/settings.js
+++ backend/src/settings.js
@@ -1,5 +1,5 @@
 export const GEMINI_API_KEY = process.env.GEMINI_API_KEY;   // placed in /.env
-export const DB_FILE_PATH = process.env.DB_FILE_PATH || './../files/db/voicer.db';
+export const DB_FILE_PATH = process.env.DB_FILE_PATH || './../local_data/db/voicer.db';
 export const VOICER_BUCKET = 'voicer';
 export const FILES_PATH = 'generated_files';
 // conversion when run via docker, localhost otherwise
