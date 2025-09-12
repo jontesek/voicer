0da6b3045479174bf45926fac06be1432a5bc75b
Jonáš Petrovský <jontesek@gmail.com>
2025-09-12 12:08:54 +0200

    fix(frontend): AppHeader: make header clickable

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="bg-primary text-white p-3 shadow-sm">
     <div class="container">
-      <h1 class="mb-0">Voicer app</h1>
+      <h1 class="mb-0"><router-link to="/">Voicer app</router-link></h1>
     </div>
   </header>
 </template>
@@ -9,5 +9,10 @@
 <style scoped>
 header {
   border-bottom: 1px solid rgba(0,0,0,0.1);
+}
+
+h1 a {
+  color: inherit;
+  text-decoration: none;
 }
 </style>
